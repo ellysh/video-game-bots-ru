f88fd7e7f21838cda22fbd01f532cc4cf0e15780
Ilya Shpigor <petrsum@gmail.com>
2019-10-26 13:12:57 +0200

    Apply Markua listings format in the "Packet Analysis" section

---
+++ manuscript/resources/code/OutGameBots/TestTcpSender.py
@@ -0,0 +1,11 @@
+import socket
+
+def main():
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+  s.settimeout(2)
+  s.connect(("127.0.0.1", 24000))
+  s.send(bytes([44, 55, 66]))
+  s.close()
+
+if __name__ == '__main__':
+  main()
---
+++ manuscript/resources/code/OutGameBots/TestUdpReceiver.py
@@ -0,0 +1,11 @@
+import socket
+
+def main():
+  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+  s.bind(("127.0.0.1", 24000))
+  data, addr = s.recvfrom(1024, socket.MSG_WAITALL)
+  print(data)
+  s.close()
+
+if __name__ == '__main__':
+  main()
